ea2186bf1a602a4ec37c678de57e97bc0e359285
joaodev <joaovaldiglesias@gmail.com>
2024-06-13 01:59:41 -0400

    creeacion base de repositorio

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+let pronombres = ['the', 'our', 'your', 'my', 'their', 'this', 'that'];
+let adjetivos = ['innovative', 'leading', 'dynamic', 'modern', 'advanced', 'creative', 'strategic', 'smart', 'efficient'];
+let sustantivos = ['solution', 'platform', 'service', 'technology', 'strategy', 'concept', 'vision', 'network', 'system', 'project', 'idea', 'enterprise', 'software', 'development', 'application', 'innovation'];
+let extensiones = ['.com', '.org', '.net', '.info', '.biz', '.io', '.edu', '.gov', '.co', '.us', '.uk', '.es', '.de', '.fr', '.it', '.ru', '.cn', '.jp', '.in', '.ca', '.au', '.br', '.mx', '.ar', '.cl'];
+
